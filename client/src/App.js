10e3c11b1b9615bf4afa0c51fb38ae9c2cc53e26
NAQVI <nomanalam7214@gmail.com>
2019-09-20 20:52:36 +0200

    change browser router to hash router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import {HashRouter as Router,Route,Switch} from "react-router-dom";
 import Login from './components/Login';
 import Main from './components/Main';
 
