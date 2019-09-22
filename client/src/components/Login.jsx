ba34781c1d0b91212ac466b928fb05634e390d73
NAQVI <nomanalam7214@gmail.com>
2019-09-22 20:57:05 +0200

    remove url from socket.io.client

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux"
 import io from "socket.io-client";
-const socket=io("http://localhost:4000")
+const socket=io()
 
 class Login extends Component {
 
