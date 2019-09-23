5d562dfab4dff3b9b8412a8293d042317a864f56
NAQVI <nomanalam7214@gmail.com>
2019-09-23 15:35:04 +0200

    .

---
+++ src/components/Login.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import {connect} from "react-redux"
+import io from "socket.io-client";
+
+const socket=io("https://mern-stack-chatapp.herokuapp.com/")
+
+class Login extends Component {
+
+    state={
+        loginname:"",
+        image:`https://joeschmoe.io/api/v1/male/`,
+        userExist:"",
+        User:{},
+        success:true
+        
+      }
+      loginUser=(e)=>{ 
+        fetch("https://mern-stack-chatapp.herokuapp.com/main/allmessages")
+        .then(res=>res.json())
+        .then(data=>{
+          
+          this.props.loadinginitialdata(data)})
+        this.setState({loginname:e.target.value},()=>{
+
+         this.props.onlineusers.map(user=>{
+
+            if(user.name===this.state.loginname){
+              this.setState({
+              userExist:"USER ALREADY EXIST",
+              success:false
+            })
+            
+            }else if(this.state.loginname.trim()!==""&& user.name!==this.state.loginname){
+            let image=this.state.image+this.state.loginname
+            const myuser={name:this.state.loginname,image:image}
+            this.setState({
+             userExist:"",
+              User:myuser,
+              success:true
+            })       
+        }
+          }) 
+        })
+        
+      }
+
+      loginsubmit=(e)=>{
+        e.preventDefault();
+        if(this.state.success&&this.state.loginname.trim()!==""){
+           socket.emit("connecteduser",this.state.User)
+        this.props.adduser(this.state.User);
+              this.props.history.push("/main") 
+      
+        }
+        else{
+          this.setState({
+            userExist:"please try again with different username"
+          })
+        }
+      }
+
+    render() {
+        
+        return (
+            <div className="login">
+            <form onSubmit={this.loginsubmit}>
+                <label> <input className="logininput" type="text" onChange={this.loginUser} /><br/>
+                </label>
+                <div className="userexist" style={{color:"white",paddingLeft:"10px",textAlign:"center"}}>{this.state.userExist}</div>
+
+                <button className="loginbtn" type="submit">Login</button>
+            </form>
+          </div>
+        )
+    }
+}
+const mapDispatchToProps=(dispatch)=>{
+    return{
+        adduser:user=>dispatch({type:"userAdded",payload:user}),
+        loadinginitialdata:data=>dispatch({type:"initialdata",payload:data})
+            }
+}
+const mapStateToProps=(state)=>{
+    return({onlineusers:state.onlineusers})
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login)
---
+++ src/components/Main.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import Userlist from './userlist';
+import Msj from "./msj"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRocket} from '@fortawesome/free-solid-svg-icons'
+import io from "socket.io-client";
+import {connect} from "react-redux";
+
+const socket=io("https://mern-stack-chatapp.herokuapp.com/")
+
+class Main extends Component {
+
+  state={
+    msj:"",
+  }
+componentDidMount(){ 
+   socket.on("usersOnline",(users)=>{
+    
+   this.props.usersAll(users) 
+  })
+  socket.on("chat",data=>{
+    this.props.addMsj(data);
+  })
+}
+
+
+  sendmsj=(e)=>{
+    e.preventDefault();
+    if(this.state.msj.trim()!==""){
+      let newmsj={name:this.props.data.username,image:this.props.data.userimage,msj:this.state.msj}
+    socket.emit("chat",newmsj)
+    this.setState({
+      msj:"",
+    })
+    }
+  }
+  scrollToBottom() {
+    const scrollHeight = this.refs.scrollme.scrollHeight;
+    const height = this.refs.scrollme.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    this.refs.scrollme.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+  }
+  
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+    render() {
+      window.addEventListener('beforeunload', (event) => {
+        socket.emit("disconnected",this.props.data.username)
+    });
+        return (
+            <>
+      {/*       <div className="userpage"> */}
+        <Userlist/>
+     {/*    </div> */}
+        <div className="container">
+      <div className="msjcontainer" ref="scrollme">
+       {this.props.data.messages.map((msj,i)=>{
+         return(  <Msj key={i} classN={this.props.data.username===msj.name?"mymsj":"msj"} username={msj.name} image={msj.image} message={msj.msj} time={new Date().toLocaleTimeString()}/> )
+       })}
+        </div> 
+        <form onSubmit={this.sendmsj} className="form">
+          <input className="input" type="text" value={this.state.msj} onChange={(e)=>this.setState({msj:e.target.value})}/>
+          <button className="btn" type="submit"><FontAwesomeIcon icon={faRocket} /></button>
+        </form>
+        </div>
+        </>
+        )
+    }
+}
+const mapStateToProps=(state)=>{
+    return({data:state})
+}
+const mapDispatchToProps=(dispatch)=>{
+  return({addMsj:msj=>dispatch({type:"addedMsj",payload:msj}),
+usersAll:user=>dispatch({type:"usersAll",payload:user})  
+}
+  )
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Main)
