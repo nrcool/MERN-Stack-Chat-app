cf3f45e628a74deea98aa5268479bc45c9151a7c
NAQVI <nomanalam7214@gmail.com>
2019-09-23 15:36:10 +0200

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {connect} from "react-redux"
 import io from "socket.io-client";
 
-const socket=io("https://mern-stack-chatapp.herokuapp.com/")
+const socket=io("https://mern-stack-chatapp.herokuapp.com")
 
 class Login extends Component {
 
